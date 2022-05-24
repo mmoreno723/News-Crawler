e2c8bb2f06b25c5825722b779dbe543f61b11678
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-05-24 17:49:00 -0400

    slightly edited javascript

--- script.js
+++ script.js
@@ -6,25 +6,22 @@ var searchButton = function(event) {
     event.preventDefault();
     var keyword = input.value.trim();
 
-    if(keyword)
-    {
-        getSearchResult(keyword);
-    }
+    getSearchResult(keyword);
 }
 
 function getSearchResult(keyword) {
     
     // start here
     var requestUrl = 'http://api.mediastack.com/v1/news?access_key=c230246a63bce12a7b4bde1321f236d3&languages=en&categories=sports';
-function getCurrentApi() {
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-}
+    function getCurrentApi() {
+        fetch(requestUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            });
+    }
 
 }
 
