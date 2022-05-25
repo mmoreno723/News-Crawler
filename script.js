2ae309d7cf7627b4735f5e01853857a6f4dd1943
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-05-25 14:39:49 -0400

    fixed search bug in JS

--- script.js
+++ script.js
@@ -8,10 +8,6 @@ var newSearchObj = {
 
 var searchButton = document.querySelector("#searchBtn");
 
-// $(function() {
-//     $('#categories').selectize();
-//   });
-
 
 var searchOnClick = function(event) {
     console.log("Search Button event listener");
@@ -21,6 +17,7 @@ var searchOnClick = function(event) {
     var categoriesOptions = document.querySelector("#categories").options;
     var categoriesSelected = [];
     var index = 0;
+    // allow support to select multiple options
     for (var i = 0; i < categoriesOptions.length; i++) {
         // add the selected options for categories
         // exclude the placeholder disabled option
@@ -37,14 +34,16 @@ var searchOnClick = function(event) {
             newSearchObj.categories += ",";
         }
     }
+    var countriesOptions = document.querySelector("#countries").value;
+    var countriesSelected = [];
+    var index = 0;
+    
 
-
-    newSearchObj.countries = document.querySelector("#countries").value;
     newSearchObj.languages = document.querySelector("#languages").value;
 
     console.log(newSearchObj);
-
-   // getSearchResults();
+    
+    getSearchResults();
 }
 
 function getMediaApi(requestUrl) {
@@ -66,7 +65,7 @@ function getSearchResults(keyword) {
         requestUrl = requestUrl + "&keywords=" + newSearchObj.keywords;
     }
     if (newSearchObj.categories != "") {
-        requestUrl = requestUrl + "&languages=" + newSearchObj.languages;
+        requestUrl = requestUrl + "&categories=" + newSearchObj.categories;
     }
     if (newSearchObj.countries != "") {
         requestUrl = requestUrl + "&countries=" + newSearchObj.countries;
